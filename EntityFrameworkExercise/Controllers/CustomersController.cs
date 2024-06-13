bc3476799863b26e40e250fb04af2f50017c9ac0
jordan <deodatojordan@gmail.com>
2024-06-12 22:18:16 -0300

    Feat: Implementando soluções para o CustomerController

--- EntityFrameworkExercise/Controllers/CustomersController.cs
+++ EntityFrameworkExercise/Controllers/CustomersController.cs
@@ -1,6 +1,8 @@
 ﻿using EntityFrameworkExercise.Data;
 using EntityFrameworkExercise.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace EntityFrameworkExercise.Controllers;
 
@@ -12,34 +14,90 @@ public class CustomersController(StoreContext context) : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
     {
-        return default;
+        if (context.Customers == null)
+        {
+            return NotFound();
+        }
+
+        return await context.Customers.ToListAsync();
     }
 
     // GET: api/Customers/5
     [HttpGet("{id}")]
     public async Task<ActionResult<Customer>> GetCustomer(int id)
     {
-        return default;
+        if (context.Customers == null)
+        {
+            return NotFound();
+        }
+        var customer = await context.Customers.FindAsync(id);
+
+        if (customer == null)
+        {
+            return NotFound();
+        }
+        return customer;
     }
 
     // PUT: api/Customers/5
     [HttpPut("{id}")]
     public async Task<IActionResult> PutCustomer(int id, Customer customer)
     {
-        return default;
-    }
+        if (id != customer.Id)
+        {
+            return BadRequest();
+        }
+        context.Entry(customer).State = EntityState.Modified;
+
+        try
+        {
+            await context.SaveChangesAsync();
+        }
+        catch (DBConcurrencyException)
+        {
+            if (!CustomerExists(id))
+            {
+                return NotFound();
+            } else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }    
 
     // POST: api/Customers
     [HttpPost]
     public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
     {
-        return default;
+        context.Customers.Add(customer);
+        await context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetCustomer), new { id = customer.Id }, customer);
     }
 
     // DELETE: api/Customers/5
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteCustomer(int id)
     {
-        return default;
+        if (context.Customers == null)
+        {
+            return NotFound();
+        }
+        var customer = await context.Customers.FindAsync(id);
+        if (customer == null)
+        {
+            return NotFound();
+        }
+        context.Customers.Remove(customer);
+        context.SaveChanges();
+
+        return NoContent();
+    }
+
+    //Método para validar a existência de um Customer
+    private bool CustomerExists(long id)
+    {
+        return (context.Customers?.Any(customer => customer.Id == id)).GetValueOrDefault();
     }
 }
